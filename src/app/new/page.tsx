3b4aec7a71c2353e3fc012b3cca6aefa3b2e5c81
jeong922 <hwajeong922@naver.com>
2023-05-03 15:32:34 +0900

    새로운 포스트 작성 기능 추가

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,3 +1,21 @@
-export default function NewPage() {
-  return <div>new</div>;
+import NewPost from '@/components/NewPost';
+import PostUserAvatar from '@/components/PostUserAvatar';
+import { authOptions } from '@/pages/api/auth/[...nextauth]';
+import { Metadata } from 'next';
+import { getServerSession } from 'next-auth';
+import { redirect } from 'next/navigation';
+
+export const metadata: Metadata = {
+  title: 'New Post',
+  description: 'Create a new post',
+};
+
+export default async function NewPage() {
+  const session = await getServerSession(authOptions);
+
+  if (!session?.user) {
+    redirect('/');
+  }
+
+  return <NewPost user={session.user} />;
 }
--- src/context/CacheKeysContext.tsx
+++ src/context/CacheKeysContext.tsx
@@ -3,6 +3,7 @@ import { createContext, useContext } from 'react';
 type CacheKeysValue = {
   postsKey: string;
 };
+
 export const CacheKeysContext = createContext<CacheKeysValue>({
   postsKey: '/api/posts',
 });
